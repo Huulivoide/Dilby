5acf210c22df79cfca448534bbb8e0e7cf79da26
Jesse Jaara <jesse.jaara@gmail.com>
2014-01-11 14:07:24 +0200

    Mark shortcuts untranslatable.

--- i18n/fi_FI.ts
+++ i18n/fi_FI.ts
@@ -4,50 +4,50 @@
 <context>
     <name>Dilby</name>
     <message>
-        <location filename="../dilby.ui" line="82"/>
+        <location filename="../dilby.ui" line="85"/>
         <source>&amp;Previous</source>
         <translation>&amp;Edellinen</translation>
     </message>
     <message>
-        <location filename="../dilby.ui" line="127"/>
+        <location filename="../dilby.ui" line="138"/>
         <source>&amp;Next</source>
         <translation>&amp;Seuraava</translation>
     </message>
     <message>
-        <location filename="../dilby.ui" line="212"/>
+        <location filename="../dilby.ui" line="204"/>
         <source>&amp;File</source>
         <translation>&amp;Tiedosto</translation>
     </message>
     <message>
-        <location filename="../dilby.ui" line="220"/>
+        <location filename="../dilby.ui" line="212"/>
         <source>&amp;Help</source>
         <translation>&amp;Apua</translation>
     </message>
     <message>
-        <location filename="../dilby.ui" line="233"/>
+        <location filename="../dilby.ui" line="227"/>
         <source>&amp;Quit</source>
         <translation>&amp;Lopeta</translation>
     </message>
     <message>
-        <location filename="../dilby.ui" line="241"/>
+        <location filename="../dilby.ui" line="240"/>
         <source>&amp;About</source>
         <translation>&amp;Tietoja</translation>
     </message>
     <message>
-        <location filename="../dilby.ui" line="246"/>
+        <location filename="../dilby.ui" line="245"/>
         <source>About &amp;QT</source>
         <translation>Tietoja &amp;Qt:stä</translation>
     </message>
     <message>
-        <location filename="../dilby.ui" line="251"/>
+        <location filename="../dilby.ui" line="250"/>
         <source>&amp;Settings</source>
         <translation>&amp;Asetukset</translation>
     </message>
     <message>
         <location filename="../dilby.cpp" line="32"/>
-        <location filename="../dilby.cpp" line="70"/>
-        <location filename="../dilby.cpp" line="83"/>
-        <location filename="../dilby.cpp" line="94"/>
+        <location filename="../dilby.cpp" line="77"/>
+        <location filename="../dilby.cpp" line="90"/>
+        <location filename="../dilby.cpp" line="101"/>
         <source>Error</source>
         <translation>Virhe</translation>
     </message>
@@ -57,29 +57,29 @@
         <translation>Virhe luotaessa välimuistikansiota: %1</translation>
     </message>
     <message>
-        <location filename="../dilby.cpp" line="70"/>
+        <location filename="../dilby.cpp" line="77"/>
         <source>Cannot open the html file for scraping.</source>
         <translation>Ladattua html tiedostoa ei voi avata stripin etsintää varten.</translation>
     </message>
     <message>
-        <location filename="../dilby.cpp" line="83"/>
+        <location filename="../dilby.cpp" line="90"/>
         <source>No image url could be scrapped.</source>
         <translation>Yhtään strippiä ei löytynyt.</translation>
     </message>
     <message>
-        <location filename="../dilby.cpp" line="94"/>
+        <location filename="../dilby.cpp" line="101"/>
         <source>Error loading the image file: %1
 Please try again.</source>
         <translation>Virhe ladattaessa kuvatiedostoa : %1\
 Yritä uudelleen.</translation>
     </message>
     <message>
-        <location filename="../dilby.cpp" line="169"/>
+        <location filename="../dilby.cpp" line="176"/>
         <source>About Dilby</source>
         <translation>Tietoja Dilby:stä</translation>
     </message>
     <message>
-        <location filename="../dilby.cpp" line="170"/>
+        <location filename="../dilby.cpp" line="177"/>
         <source>&lt;h1&gt;Dilby&lt;/h1&gt;The source code can be found at:&lt;br&gt;%1&lt;br&gt; To contact send email to:&lt;br&gt;%2</source>
         <translation>&lt;h1&gt;Dilby&lt;/h1&gt;Lähdekoodi on saatavilla osoitteessa:&lt;br&gt;%1&lt;br&gt;Sähköpostia voi lähettää osoitteeseen:&lt;br&gt;%2</translation>
     </message>
@@ -105,24 +105,49 @@ Yritä uudelleen.</translation>
 <context>
     <name>SettingsDialog</name>
     <message>
-        <location filename="../settingsdialog.ui" line="31"/>
-        <source>Dilby settings:</source>
-        <translation>Dilby:n asetukset:</translation>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>Settings</source>
+        <translation>Asetukset</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.ui" line="45"/>
+        <location filename="../settingsdialog.ui" line="20"/>
         <source>Use high quality comics</source>
         <translation>Käytä korkealaatusia strippejä</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.ui" line="74"/>
+        <location filename="../settingsdialog.ui" line="56"/>
+        <source>&amp;Clear</source>
+        <translation>&amp;Tyhjennä</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="87"/>
         <source>&amp;OK</source>
         <translation>&amp;OK</translation>
     </message>
     <message>
-        <location filename="../settingsdialog.ui" line="81"/>
+        <location filename="../settingsdialog.ui" line="94"/>
         <source>&amp;Cancel</source>
         <translation>&amp;Peruuta</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="19"/>
+        <source>Calculating cache size.</source>
+        <translation>Lasketaan välimuistin kokoa.</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="53"/>
+        <source>MB</source>
+        <translation>Mt</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="58"/>
+        <source>KB</source>
+        <translation>Kt</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="61"/>
+        <source>Cache size: %1%2</source>
+        <translation>Välimuistin koko:%1%2</translation>
     </message>
 </context>
 </TS>
